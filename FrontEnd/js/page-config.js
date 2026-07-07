62b421b821b3ac3f4d4e245b0dd112abcd0009e3
Leonardo Fernandes Causin <leonardocausin5@gmail.com>
2026-07-07 17:30:39 -0300

    teste

--- FrontEnd/js/page-config.js
+++ FrontEnd/js/page-config.js
@@ -6,7 +6,7 @@
     viewCacheMaxAge: 10 * 60 * 1000,
     viewCachePrefix: "zentrix-view-cache:",
     viewStatePrefix: "zentrix-view-state:",
-    clientCacheVersion: "20260707-topbar-pro-2",
+    clientCacheVersion: "20260707-topbar-pro-3-3",
     prefetchPeriods: Object.freeze(["today", "7d", "month", "year"])
   });
 })();
